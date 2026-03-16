3cad718c6b9e1ca1406369b4a9fbe4b1596900e5
Sajjadul Islam <sajjadul625@gmail.com>
2026-03-17 00:34:56 +0600

    ques , input , output

---
+++ Lab-3/20244103008.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "What is your favorite color?"
+read color
+echo "Favorite Color: $color"
+
+echo "What is your age?"
+read age
+echo "Age: $age"
+
+echo "What is your hobby?"
+read hobby
+echo "Hobby: $hobby"
+
+echo "Which city do you live in?"
+read city
+echo "City: $city"
+
