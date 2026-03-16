021a1db82560af86ca67506b9e9873566aa173e5
Sajjadul Islam <sajjadul625@gmail.com>
2026-03-17 00:41:57 +0600

    Operations

---
+++ Lab-3/20244103008-Operations.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter first number:"
+read num1
+
+echo "Enter second number:"
+read num2
+
+echo "Addition of $num1 and $num2: $((num1 + num2))"
+echo "Subtraction of $num1 and $num2: $((num1 - num2))"
+echo "Multiplication of $num1 and $num2: $((num1 * num2))"
+echo "Division of $num1 and $num2: $((num1 / num2))"
