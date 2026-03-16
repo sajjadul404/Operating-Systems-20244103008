b92cf12d1f7c62e14dda597f309e1d451ac1054b
Sajjadul Islam <sajjadul625@gmail.com>
2026-03-17 00:44:30 +0600

    Task 1

---
+++ Lab-4/Task1_3008.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+read -p "Enter a number: " n
+
+for ((i=1; i<=n; i++))
+do
+  for ((j=1; j<=i; j++))
+  do
+    echo -n "*"
+  done
+  echo
+done
+
+for ((i=n-1; i>=1; i--))
+do
+  for ((j=1; j<=i; j++))
+  do
+    echo -n "*"
+  done
+  echo
+done
